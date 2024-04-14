185e3690b664232c074c35016579bf865a99cf47
runner <runner@Mac-1713119130042.local>
2024-04-14 20:08:14 +0000

    Update Package.swift

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TensorFlowLiteC",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(
+            name: "TensorFlowLiteC",
+            targets: [
+                "TensorFlowLiteC",
+                "TensorFlowLiteCCoreML",
+                "Link"
+            ]
+        )
+    ],
+    targets: [
+        .target(
+            name: "Link",
+            linkerSettings: [.linkedLibrary("c++")]
+        ),
+        .binaryTarget(
+            name: "TensorFlowLiteC",
+            url: "https://api.github.com/repos/tareksabry1337/TensorFlowLiteC/releases/assets/162066280.zip",
+            checksum: "20d1905a420e13412f622185e89a51a8ec10fe4f953c277676cce3c4cc290b78"
+        ),
+        .binaryTarget(
+            name: "TensorFlowLiteCCoreML",
+            url: "https://api.github.com/repos/tareksabry1337/TensorFlowLiteC/releases/assets/162066274.zip",
+            checksum: "87fd5970b2cea6f508b76c5859e11b16ee63bbf66807cb70aca38897c759bf41"
+        )
+    ]
+)
