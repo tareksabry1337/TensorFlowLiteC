4b7eff59accf880d7dd1e33fa34ff4cce0b6e70a
Anka <runner@Mac-1715168079279.local>
2024-05-08 11:58:01 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,13 +23,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "TensorFlowLiteC",
-            url: "https://api.github.com/repos/tareksabry1337/TensorFlowLiteC/releases/assets/166710566.zip",
-            checksum: "c80bac956eccbd7dfd5aa121530f3277db137d06fc858816ea07356e06ead399"
+            url: "https://api.github.com/repos/tareksabry1337/TensorFlowLiteC/releases/assets/166724021.zip",
+            checksum: "a83c0845f034bf7477c895fb0a85394f492e70422008ed83fcc6a025d507d771"
         ),
         .binaryTarget(
             name: "TensorFlowLiteCCoreML",
-            url: "https://api.github.com/repos/tareksabry1337/TensorFlowLiteC/releases/assets/166710559.zip",
-            checksum: "38ec59993db37aa27ed636fab6d3f9bbddd84fae5dd0757df71d6157470dfa23"
+            url: "https://api.github.com/repos/tareksabry1337/TensorFlowLiteC/releases/assets/166724019.zip",
+            checksum: "82a1952d5f736d0b903bc078db677a2e97aae5b2d21de5e8b62539bf8dbf8ea2"
         )
     ]
 )
